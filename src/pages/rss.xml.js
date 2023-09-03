71c797e8ae08567df56f31c9964697509858e3af
JohnBakhmat <johnbakhmat@gmail.com>
2023-09-03 19:11:08 +0300

    fixed rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,18 @@ import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
 	const posts = await getCollection("blog");
+	
+	const items = posts.map((post) => ({
+		...post.data,
+		draft: undefined,
+		link: `/blog/${post.slug}/`,
+	}))
+
+	console.dir(items)
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
+		items: items,
 	});
 }
