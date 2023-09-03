ea254ce0c821fd732f60dd731924eee5add193ce
JohnBakhmat <johnbakhmat@gmail.com>
2023-09-03 18:31:58 +0300

    New colors

--- uno.config.ts
+++ uno.config.ts
@@ -1,23 +1,24 @@
 import {
-    defineConfig,
-    presetUno,
-    transformerDirectives,
-    transformerVariantGroup,
-  } from "unocss";
-  import { presetScrollbar } from "unocss-preset-scrollbar";
-  
-  export default defineConfig({
-    presets: [presetUno(), presetScrollbar()],
-    transformers: [transformerDirectives(), transformerVariantGroup()],
-    theme: {
-      fontFamily: {
-          martian: "Martian Mono",
-          mono: "Martian Mono",
-      },
-      colors: {
-          themeText:"#FF7900",
-          vitesseGreen: "#4d9375",
-          themeBlack: "#121212",
-      },
-    },
-  });
+	defineConfig,
+	presetUno,
+	transformerDirectives,
+	transformerVariantGroup,
+} from "unocss";
+import { presetScrollbar } from "unocss-preset-scrollbar";
+
+export default defineConfig({
+	presets: [presetUno(), presetScrollbar()],
+	transformers: [transformerDirectives(), transformerVariantGroup()],
+	theme: {
+		fontFamily: {
+			martian: "Martian Mono",
+			mono: "Martian Mono",
+		},
+		colors: {
+			themeText: "#E69C51",
+			vitesseGreen: "#4d9375",
+			themeBlack: "#121212",
+			themeAccent: "#BD7EE1",
+		},
+	},
+});
