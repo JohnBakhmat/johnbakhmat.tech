d29eb180a3f80b37f7000fcc102d94215d15bf7d
Yevhenii <yevhenii.bakhmat@nure.ua>
2023-09-04 19:51:43 +0300

    add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,17 @@ import react from "@astrojs/react";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { loadTheme } from "shiki-themes";
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://johnbakhmat-dev.vercel.app",
-	integrations: [unocss(), mdx(), sitemap(), react()],
-	markdown: {
-		extendDefaultPlugins: true,
-		rehypePlugins: [rehypeAutolinkHeadings],
-		shikiConfig: {
-			theme: loadTheme("./vesper-theme.json"),
-		},
-	},
+  site: "https://johnbakhmat-dev.vercel.app",
+  integrations: [unocss(), mdx(), sitemap(), react(), prefetch()],
+  markdown: {
+    extendDefaultPlugins: true,
+    rehypePlugins: [rehypeAutolinkHeadings],
+    shikiConfig: {
+      theme: loadTheme("./vesper-theme.json")
+    }
+  }
 });
