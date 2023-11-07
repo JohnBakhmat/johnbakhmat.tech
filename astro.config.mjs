bd3cf1b93285292c4d79cc162cbc6ab26c1086e0
Yevhenii <yevhenii.bakhmat@nure.ua>
2023-11-07 12:25:13 +0300

    analytics?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import unocss from "unocss/astro";
 import react from "@astrojs/react";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import vercel from "@astrojs/vercel/serverless";
 import { loadTheme } from "shiki-themes";
 
 import prefetch from "@astrojs/prefetch";
@@ -11,6 +12,12 @@ import prefetch from "@astrojs/prefetch";
 // https://astro.build/config
 export default defineConfig({
   site: "https://johnbakhmat-dev.vercel.app",
+  output: "server",
+  adapter: vercel({
+    webAnaytics: {
+      enabled: true
+    }
+  }),
   integrations: [unocss(), mdx(), sitemap(), react(), prefetch()],
   markdown: {
     extendDefaultPlugins: true,
