88e34b1abd7f69e3c78731aa25ab524993e2398d
John Bakhmat <johnbakhmat@gmail.com>
2024-01-14 12:35:44 +0300

    Handle project api breaks

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,5 +11,4 @@ const blog = defineCollection({
 	}),
 });
 
-
 export const collections = { blog };
--- src/utils/read-time.ts
+++ src/utils/read-time.ts
@@ -1,12 +1,11 @@
 import getReadingtime from "reading-time";
 import type { CollectionEntry } from "astro:content";
 
-
-type Post = CollectionEntry<'blog'>
-export const addReadTimes = (p:Post):Post => ({
-  ...p,
-  data: {
-    ...p.data,
-    readingTime: getReadingtime(p.body).text,
-  },
-})
+type Post = CollectionEntry<"blog">;
+export const addReadTimes = (p: Post): Post => ({
+	...p,
+	data: {
+		...p.data,
+		readingTime: getReadingtime(p.body).text,
+	},
+});
