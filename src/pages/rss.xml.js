bfb5406bd46d6af39fbd5a5300b4dcd56c8cedaa
John Bakhmat <johnbakhmat@gmail.com>
2024-01-30 13:35:32 +0300

    made projects load after page load

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,19 +3,19 @@ import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection("blog");
-	
-	const items = posts.map((post) => ({
-		...post.data,
-		draft: undefined,
-		link: `/blog/${post.slug}/`,
-	}))
+  const posts = await getCollection("blog");
 
-	console.dir(items)
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: items,
-	});
+  const items = posts.map((post) => ({
+    ...post.data,
+    draft: undefined,
+    link: `/blog/${post.slug}/`,
+  }));
+
+  console.dir(items);
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: items,
+  });
 }
