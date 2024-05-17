699d3b8b6fa62529ed8772a0228d5788131ba751
John Bakhmat <johnbakhmat@gmail.com>
2024-05-17 17:10:23 +0300

    reformat

--- src/components/Age.tsx
+++ src/components/Age.tsx
@@ -1,25 +1,25 @@
 import { useState, useEffect } from "react";
 
 function getAge() {
-  return (
-    (new Date().getTime() - new Date(2002, 0, 9).getTime()) /
-    (1000 * 60 * 60 * 24 * 365.25)
-  )
-    .toString()
-    .slice(0, 12);
+    return (
+        (new Date().getTime() - new Date(2002, 0, 9).getTime()) /
+        (1000 * 60 * 60 * 24 * 365.25)
+    )
+        .toString()
+        .slice(0, 12);
 }
 
 export const Age = () => {
-  const [age, setAge] = useState(getAge());
+    const [age, setAge] = useState(getAge());
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setAge(getAge());
-    });
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setAge(getAge());
+        });
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
-  return <span className="font-mono">{age}</span>;
+    return <span className="font-mono">{age}</span>;
 };
--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -2,54 +2,57 @@ import type { Repo } from "../repo";
 import { Project } from "./Project.tsx";
 
 type Response = {
-  Name: string;
-  Description: string;
-  Url: string;
-  Stars: number;
-  Forks: number;
-  Languages: string[];
+    Name: string;
+    Description: string;
+    Url: string;
+    Stars: number;
+    Forks: number;
+    Languages: string[];
 }[];
 
 const pinnedRepos = await fetch("https://pinned.fly.dev/projects/johnbakhmat")
-  .then((r) => r.json())
-  .then((json) => json as Response)
-  .then((r) =>
-    r.map(
-      (i) =>
-        ({
-          name: i.Name,
-          description: i.Description,
-          url: i.Url,
-          stars: i.Stars,
-          forks: i.Forks,
-          languages: i.Languages,
-        }) as Repo,
-    ),
-  )
-  .catch((error) => {
-    console.debug("ERR", error);
-    return [];
-  });
+    .then((r) => r.json())
+    .then((json) => json as Response)
+    .then((r) =>
+        r.map(
+            (i) =>
+                ({
+                    name: i.Name,
+                    description: i.Description,
+                    url: i.Url,
+                    stars: i.Stars,
+                    forks: i.Forks,
+                    languages: i.Languages,
+                }) as Repo,
+        ),
+    )
+    .catch((error) => {
+        console.error("Failed to fetch pinned repos: ", error);
+        return [];
+    });
+
+function NoRepo() {
+    return (
+        <div className="col-span-full row-span-full flex flex-col place-content-center text-center">
+            <p>
+                OOps can't get project info! If you see this,{" "}
+                <a href="mailto:johnbakhmat@gmail.com/?subject=Yo, your site project api broke">
+                    contact me
+                </a>{" "}
+                ASAP!
+            </p>
+            <h1>☹</h1>
+        </div>
+    );
+}
+
+const HasRepos = (repos) =>
+    pinnedRepos.map((repo) => <Project repo={repo} key={repo.name} />);
 
 export default function ProjectSection() {
-  return (
-    <section className="w-full grid grid-cols-1 gap-4 auto-cols-max sm:grid-cols-2 sm:gap-3 place-items-center">
-      {pinnedRepos.length > 0 ? (
-        pinnedRepos.map((repo) => {
-          return <Project repo={repo} key={repo.name} />;
-        })
-      ) : (
-        <div className="col-span-full row-span-full flex flex-col place-content-center text-center">
-          <p>
-            OOps can't get project info! If you see this,{" "}
-            <a href="mailto:johnbakhmat@gmail.com/?subject=Yo, your site project api broke">
-              contact me
-            </a>{" "}
-            ASAP!
-          </p>
-          <h1>☹</h1>
-        </div>
-      )}
-    </section>
-  );
+    return (
+        <section className="w-full grid grid-cols-1 gap-4 auto-cols-max sm:grid-cols-2 sm:gap-3 place-items-center">
+            {pinnedRepos.length > 0 ? HasRepos(pinnedRepos) : NoRepo()}
+        </section>
+    );
 }
