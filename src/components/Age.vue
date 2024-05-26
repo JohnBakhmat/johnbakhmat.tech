14402ff3e6cd47139b99babb04aa315da48ac7dc
John Bakhmat <johnbakhmat@gmail.com>
2024-05-26 17:42:15 +0300

    remove age, blogview and post (react)

--- src/components/Age.vue
+++ src/components/Age.vue
@@ -3,8 +3,7 @@ import { ref, onUnmounted } from "vue";
 
 const age = ref(new Date());
 const birthday = new Date(2002, 0, 9).getTime();
-
-let interval = setInterval(() => {
+const interval = setInterval(() => {
   age.value = ((new Date().getTime() - birthday) / 31557600000) // 1000 * 60 * 60 * 24 * 365.25
     .toString()
     .slice(0, 12);
