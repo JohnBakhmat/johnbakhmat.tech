c3652d9ce967aa82cb189eb230012763d93786dd
John Bakhmat <johnbakhmat@gmail.com>
2024-06-06 09:36:33 +0300

    fixing font causing layout shift

--- src/components/Age.vue
+++ src/components/Age.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onUnmounted } from "vue";
 
-const age = ref(new Date());
+const age = ref("00.000000000");
 const birthday = new Date(2002, 0, 9).getTime();
 const interval = setInterval(() => {
   age.value = ((new Date().getTime() - birthday) / 31557600000) // 1000 * 60 * 60 * 24 * 365.25
