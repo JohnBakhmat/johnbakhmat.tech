4403a2db13f2d6dbfd77a22d622f97f232c8e471
John Bakhmat <johnbakhmat@gmail.com>
2024-06-17 11:03:49 +0300

    astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,30 +5,31 @@ import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { loadTheme } from "shiki-themes";
 import unocss from "unocss/astro";
-
 import vue from "@astrojs/vue";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://johnbakhmat.tech",
-    output: "server",
-    adapter: node({
-        mode: "standalone",
-    }),
-    integrations: [unocss(), mdx(), sitemap(), vue()],
-    markdown: {
-        extendDefaultPlugins: true,
-        rehypePlugins: [rehypeAutolinkHeadings],
-        shikiConfig: {
-            theme: loadTheme("./vesper-theme.json"),
-        },
+  site: "https://johnbakhmat.tech",
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  integrations: [unocss(), mdx(), sitemap(), vue(), compress()],
+  markdown: {
+    extendDefaultPlugins: true,
+    rehypePlugins: [rehypeAutolinkHeadings],
+    shikiConfig: {
+      theme: loadTheme("./vesper-theme.json"),
     },
-    prefetch: {
-        prefetchAll: true,
-        defaultStrategy: "viewport",
-    },
-    experimental: {
-        clientPrerender: true,
-        actions: true,
-    },
+  },
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
+  experimental: {
+    clientPrerender: true,
+    actions: true,
+  },
 });
