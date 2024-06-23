2b39bfaeae7cf5ea253153356ed7f7800462c8e4
John Bakhmat <johnbakhmat@gmail.com>
2024-06-23 23:57:04 +0300

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,34 +6,35 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { loadTheme } from "shiki-themes";
 import unocss from "unocss/astro";
 import vue from "@astrojs/vue";
-
 import compress from "astro-compress";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://johnbakhmat.tech",
   output: "server",
   adapter: node({
-    mode: "standalone",
+    mode: "standalone"
   }),
-  integrations: [unocss(), mdx(), sitemap(), vue(), compress()],
+  integrations: [unocss(), mdx(), sitemap(), vue(), compress(), svelte()],
   markdown: {
     extendDefaultPlugins: true,
     rehypePlugins: [rehypeAutolinkHeadings],
     shikiConfig: {
-      theme: loadTheme("./vesper-theme.json"),
-    },
+      theme: loadTheme("./vesper-theme.json")
+    }
   },
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: "viewport",
+    defaultStrategy: "viewport"
   },
   experimental: {
     clientPrerender: true,
-    actions: true,
+    actions: true
   },
   redirects: {
     "/tools": "/uses",
-    "/blog/[...slug]": "/articles/[...slug]",
-  },
+    "/blog/[...slug]": "/articles/[...slug]"
+  }
 });
