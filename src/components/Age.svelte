048600f81725f0e7c935a08dde9aec1b80048ad8
John Bakhmat <johnbakhmat@gmail.com>
2024-06-24 09:14:21 +0300

    Tried svelte for age

---
+++ src/components/Age.svelte
@@ -0,0 +1,18 @@
+<script>
+  let age = "00.000000000";
+  const birthday = new Date(2002, 0, 9).getTime();
+  const getAge = () => {
+    age = ((new Date().getTime() - birthday) / 31557600000)
+      .toString()
+      .slice(0, 12);
+  };
+
+  let interval;
+
+  $: {
+    clearInterval(interval);
+    interval = setInterval(getAge, 50);
+  }
+</script>
+
+<span class="font-mono">{age}</span>
